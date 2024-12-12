4d3ac4cadab4cdb3fe607c8e38fcc560adef3a90
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2024-12-12 18:42:56 -0500

    new parser

--- src/assembler.rs
+++
@@ -1,9 +0,0 @@
-pub struct ObjectFile {
-    text: Vec<i32>,
-    data: Vec<i32>,
-}
-
-pub fn assemble(src: &str) -> Option<ObjectFile> {
-    let lines = src.lines();
-    None
-}
--- src/lc2k.rs
+++ src/lc2k.rs
@@ -1,131 +1,97 @@
-use std::str::SplitWhitespace;
-
-use error_chain::example_generated::{Result, ResultExt};
-
 pub const MEM_SIZE: usize = 65536;
 pub const NUM_REG: usize = 8;
 
-#[derive(Debug)]
 pub struct Register(usize);
 
 impl Register {
-    pub fn from_str(src: &str) -> Result<Self> {
-        let r = src.parse::<usize>().chain_err(|| "Failed to parse register")?;
-
-        if (0..NUM_REG).contains(&r) {
-            Ok(Register(r))
+    pub fn from_str(src: &str) -> Self {
+        let u = src.parse().unwrap();
+        if u >= NUM_REG {
+            unreachable!("Register too big");
         }
-        else {
-            Err("Register number out of range".into())
-        }
+        Register(u)
     }
 }
 
-#[derive(Debug)]
-pub struct Label(String);
-
-impl Label {
-    pub fn from_str(src: &str) -> Option<Self> {
-        match src.chars().next() {
-            Some(ch) if ch.is_alphabetic() => Some(Label(src.into())),
-            _ => None
-        }
-    }
-}
-
-#[derive(Debug)]
 pub enum Address {
     Literal(i32),
-    Label(Label),
+    Symbol(String),
 }
 
 impl Address {
-    pub fn from_str(src: &str) -> Result<Self> {
-        let label = Label::from_str(src);
-
-        match label {
-            Some(l) => Ok(Address::Label(l)),
-            _ => Ok(Self::Literal(src.parse().chain_err(|| "Cannot parse address")?))
+    pub fn from_str(src: &str) -> Self {
+        if src.chars().next().unwrap().is_alphabetic() {
+            Self::Symbol(src.to_string())
+        }
+        else {
+            Self::Literal(src.parse().unwrap())
         }
     }
 }
 
-#[derive(Debug)]
 pub enum Instruction {
     Add(Register, Register, Register),
     Nor(Register, Register, Register),
 
-    // Relocation
     Lw(Register, Register, Address),
     Sw(Register, Register, Address),
     Beq(Register, Register, Address),
 
     Jalr(Register, Register),
+
     Halt,
     Noop,
 
     Fill(i32),
 }
 
-impl Instruction {
-    pub fn from_str(mut src: SplitWhitespace) -> Result<Instruction> {
-        match src.next() {
-            Some("add") => Ok(
-                Self::Add(Register::from_str(src.next().chain_err(|| "Expected register 1")?)?,
-                          Register::from_str(src.next().chain_err(|| "Expected register 2")?)?, 
-                          Register::from_str(src.next().chain_err(|| "Expected register 3")?)?
-                )),
-            Some("nor") => Ok(
-                Self::Nor(Register::from_str(src.next().chain_err(|| "Expected register 1")?)?,
-                          Register::from_str(src.next().chain_err(|| "Expected register 2")?)?, 
-                          Register::from_str(src.next().chain_err(|| "Expected register 3")?)?
-                )),
-            Some("lw") => Ok(
-                Self::Lw(Register::from_str(src.next().chain_err(|| "Expected register 1")?)?,
-                          Register::from_str(src.next().chain_err(|| "Expected register 2")?)?, 
-                          Address::from_str(src.next().chain_err(|| "Expected address")?)?
-                )),
-            Some("sw") => Ok(
-                Self::Lw(Register::from_str(src.next().chain_err(|| "Expected register 1")?)?,
-                          Register::from_str(src.next().chain_err(|| "Expected register 2")?)?, 
-                          Address::from_str(src.next().chain_err(|| "Expected address")?)?
-                )),
-            Some("beq") => Ok(
-                Self::Lw(Register::from_str(src.next().chain_err(|| "Expected register 1")?)?,
-                          Register::from_str(src.next().chain_err(|| "Expected register 2")?)?, 
-                          Address::from_str(src.next().chain_err(|| "Expected address")?)?
-                )),
-            Some("jalr") => Ok(
-                Self::Jalr(Register::from_str(src.next().chain_err(|| "Expected register 1")?)?,
-                          Register::from_str(src.next().chain_err(|| "Expected register 2")?)?, 
-                )),
-            Some("halt") => Ok(Self::Halt),
-            Some("noop") => Ok(Self::Noop),
-            Some(".fill") => Ok(Self::Fill(
-                    src.next().chain_err(|| "Expected value")?.parse()
-                    .chain_err(|| "Fill value must be an integer")?
-                    )),
-            None => Err("Empty instruction".into()),
-            _ => Err("Invlid instruction".into())
+pub fn parse(src: &str) -> Vec<(Option<String>, Instruction)> {
+    src.lines().map(|line| {
+        let mut words = line.split_whitespace();
+
+        let label = words.next().unwrap();
+        let label = if label.is_empty() {
+            None
         }
-    }
-}
+        else {
+            Some(label.to_string())
+        };
 
-pub fn parse_line(src: &str) -> Result<(Option<Label>, Instruction)> {
-    let mut src = src.split_whitespace(); 
-        Ok((
-        Label::from_str(src.next().chain_err(|| "Unexpected end of line")?),
-        Instruction::from_str(src)?
-        ))
-}
-
-#[cfg(test)]
-mod tests {
-    use super::{parse_line, Instruction};
-
-    #[test]
-    fn parse() {
-        let i = parse_line("main lw 1 1 label");
-        print!("{:?}", i)
-    }
+        let instr = match words.next().unwrap() {
+            "add" => Instruction::Add(
+                    Register::from_str(words.next().unwrap()),
+                    Register::from_str(words.next().unwrap()),
+                    Register::from_str(words.next().unwrap())
+                ),
+            "nor" => Instruction::Nor(
+                    Register::from_str(words.next().unwrap()),
+                    Register::from_str(words.next().unwrap()),
+                    Register::from_str(words.next().unwrap())
+                ),
+            "lw" => Instruction::Lw(
+                    Register::from_str(words.next().unwrap()),
+                    Register::from_str(words.next().unwrap()),
+                    Address::from_str(words.next().unwrap())
+                ),
+            "sw" => Instruction::Sw(
+                    Register::from_str(words.next().unwrap()),
+                    Register::from_str(words.next().unwrap()),
+                    Address::from_str(words.next().unwrap())
+                ),
+            "beq" => Instruction::Beq(
+                    Register::from_str(words.next().unwrap()),
+                    Register::from_str(words.next().unwrap()),
+                    Address::from_str(words.next().unwrap())
+                ),
+            "jalr" => Instruction::Jalr(
+                    Register::from_str(words.next().unwrap()), 
+                    Register::from_str(words.next().unwrap())
+                ),
+            "halt" => Instruction::Halt,
+            "fill" => Instruction::Fill(words.next().unwrap().parse().unwrap()),
+            "noop" => Instruction::Noop,
+            _ => unreachable!("Bad instruction")
+        };
+        (label, instr)
+    }).collect()
 }
